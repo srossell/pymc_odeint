19b66f2ff3a381b640fdba0c098ccb88eea1b139
Sergio Rossell <sergio.rossell@gmail.com>
2018-03-28 23:10:03 +0200

    Add scatter matrix plot to pk example Changes to be committed: 	modified:   pkmodel/pk_fit.py

--- pkmodel/pk_fit.py
+++ pkmodel/pk_fit.py
@@ -1,4 +1,6 @@
 import pymc
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # load the model
 import pk_model as mod
@@ -6,4 +8,9 @@ import pk_model as mod
 # fit the model with mcmc
 mc = pymc.MCMC(mod)
 mc.use_step_method(pymc.AdaptiveMetropolis, [mod.kcp, mod.kpc, mod.ke])
-mc.sample(iter=2000, burn=1000, thin=20, verbose=1)
+mc.sample(iter=50000, burn=10000, thin=2, verbose=1)
+
+df = pd.DataFrame({'kcp': mc.trace('kcp')[:], 'kpc': mc.trace('kpc')[:],
+                    'ke': mc.trace('ke')[:]})
+pd.plotting.scatter_matrix(df)
+plt.show()

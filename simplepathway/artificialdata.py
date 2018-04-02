1437b6d4125567497b9579e5bba99a180841eaec
Sergio Rossell <sergio.rossell@gmail.com>
2018-04-02 17:12:43 +0200

    Improve integration between scripts and notebooks
    Changes to be committed:
    	modified:   simplepathway/__pycache__/model.cpython-36.pyc
    	modified:   simplepathway/artificialdata.py
    	modified:   simplepathway/compare_leastsquares_mcmc_simplepathway.ipynb
    	modified:   simplepathway/model.py

--- simplepathway/artificialdata.py
+++ simplepathway/artificialdata.py
@@ -10,16 +10,15 @@ np.random.seed(42)
 init = [0.]
 time_vec = np.arange(0, 10, 1)
 p_vec = [10, 1]
+v_in = 9
 
-def odes(x, t, p):
+def odes(x, t, p, v_in):
     Vmax = p[0]
     Km = p[1]
     return np.array([
-        9 - Vmax * x[0] / (Km + x[0]),
+        v_in - Vmax * x[0] / (Km + x[0]),
         ])
 
-xt = odeint(odes, init, time_vec, args=(p_vec,))
+xt = odeint(odes, init, time_vec, args=(p_vec, v_in))
 obs = xt.flatten() + np.random.randn(xt.flatten().shape[0])*0.15
 
-# xt.flatten() + np.random.randn(xt.flatten().shape[0])
-# obs = [1.096, 4.184, 3.195, 5.626, 5.832, 7.460, 8.107, 6.426, 7.563, 8.212]
--- simplepathway/model.py
+++ simplepathway/model.py
@@ -2,32 +2,26 @@ import pymc as pm
 import numpy as np
 from scipy.integrate import odeint
 
-time = np.arange(0, 10, 1)
-
-obs = np.array([1.096, 4.184, 3.195, 5.626, 5.832, 7.460, 8.107, 6.426, 7.563,
-                8.212])
+from artificialdata import init
+from artificialdata import obs
+from artificialdata import odes
+from artificialdata import p_vec
+from artificialdata import time_vec
+from artificialdata import v_in
 
 Vmax = pm.Uniform('Vmax', 0., 100.)
 Km = pm.Uniform('Km', 0., 10.)
 
-tspan = time
-y0 = [0.]
+init = [0.]
 
 # Deterministic model
 @pm.deterministic
 def mymodel(p0=Vmax, p1=Km):
-    def odes(y, t):
-        return ([
-                    9 - p0 * y[0] / (p1 + y[0]),
-                ])
-    soln = odeint(odes, y0, tspan)
+    soln = odeint(odes, init, time_vec, args=([p0, p1], v_in))
     xt = soln.flatten()
     return xt
 xt = pm.Lambda('xt', lambda mymodel=mymodel: mymodel)
 
-prec = pm.Gamma('precision', alpha=0.1, beta=0.1)
-
 # data likelihood
 A = pm.Normal('A', mu=xt, tau=1, value=obs, observed=True)
-
 
